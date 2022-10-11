0e0592e8137a1528051d6c07328a72e2f9bb20a9
NuCodes <74950695+NupookDev@users.noreply.github.com>
2022-10-11 11:28:03 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const map = L.map('map')
+const map = L.map('map').setView([51.505, -0.09], 13)
 
 L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=jOTlKffECGLIDKDu2YxN', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
